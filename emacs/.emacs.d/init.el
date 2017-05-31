fad58c916f25a74baf424c8f82f963a4d31865c5
Jacob Pavlock <jtpavlock@gmail.com>
2017-05-30 23:40:34 -1000

    magit

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -8,7 +8,7 @@
     ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
  '(package-selected-packages
    (quote
-    (systemd evil-surround evil-nerd-commenter org-bullets evil-tutor evil flx smex counsel ivy general which-key linum-relative zenburn-theme smart-mode-line use-package))))
+    (magit systemd evil-surround evil-nerd-commenter org-bullets evil-tutor evil flx smex counsel ivy general which-key linum-relative zenburn-theme smart-mode-line use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
